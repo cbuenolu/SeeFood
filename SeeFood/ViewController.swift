7bc287d65be1e59bfdc04065ee599b8e0b200c13
Carlos Bueno <carlos.bueno@opinno.com>
2018-10-24 12:07:05 +0200

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Carlos Miguel Bueno Lujan on 24/10/2018.
+//  Copyright © 2018 Carlos Miguel Bueno Lujan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
